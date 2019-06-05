c78a53272a220fcb169ae671b2c1a6cef89d9b24
Cosmin <cosminmartinc@gmail.com>
2019-06-06 00:26:08 +0300

    Added App.js and components

---
+++ React/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h3>Social Media</h3>
+            <ul>
+                <li>Facebook</li>
+                <li>Linkedin</li>
+                <li>Youtube</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ React/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='navbar'>
+            <h1>New Site</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
+
+
